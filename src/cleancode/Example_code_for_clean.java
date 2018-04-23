3d6a1568448319d817f772c8f67a89f76389de79
Pcharlet <charletpierre6@gmail.com>
2018-04-23 11:43:09 +0200

    code (sale) implementé

---
+++ src/cleancode/Example_code_for_clean.java
@@ -0,0 +1,19 @@
+package cleancode;
+
+public class Example_code_for_clean {
+	
+	String ApmNuter ="Les Misérables";
+	char n = '1';
+	char n2 ='5';
+	char n3='9';	
+	
+	public void FindBY(String nom) {// fait une chercher par nom et pas par id
+	if (nom=="pierre") {System.out.println("ok c'est bon");}else {System.out.println("c'est pas bon");}
+	}
+	
+	public String Find(String id) {// fait une chercher par id
+	id = null;
+	return id;
+	}
+
+}
