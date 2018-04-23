a7865022299fcaa03db9dfcebfab9a3a3da94cf8
Pcharlet <charletpierre6@gmail.com>
2018-04-23 11:49:45 +0200

    code (sale) corriger

--- src/cleancode/Example_code_for_clean.java
+++ src/cleancode/Example_code_for_clean.java
@@ -16,4 +16,31 @@ public class Example_code_for_clean {
 	return id;
 	}
 
+	
+	//correction
+	
+	// String NomLivre = "Les Misérables";
+	//char pageDébut = '1';
+	//char pageMilieu ='5';
+	//char pageFin='9';	
+	
+	//public void FindBYNom(String nom) {
+	//if (nom=="pierre") {
+	//	System.out.println("ok c'est bon");
+	//}
+	//else{
+	//	System.out.println("c'est pas bon");
+	//}
+	//}
+	
+	//public String FindById(String id) {
+	//	try {
+	//		id = null;
+	//		return id;
+	//	}catch (Error ex){
+	//		String messerror = "id null";
+	//		return messerror;
+	//	}
+	//}
+
 }
