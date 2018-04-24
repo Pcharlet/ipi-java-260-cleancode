91805c780f95e3ac3c370ee8bce3e697cd81cb5f
Pcharlet <charletpierre6@gmail.com>
2018-04-24 23:22:49 +0200

    correction clean code

--- src/cleancode/Example_code_for_clean.java
+++ src/cleancode/Example_code_for_clean.java
@@ -2,19 +2,21 @@ package cleancode;
 
 public class Example_code_for_clean {
 	
-	String ApmNuter ="Les Misérables";
+	String ApmNuter ="Les Misérables"; //nom de variable nom explicite
 	char n = '1';
 	char n2 ='5';
 	char n3='9';	
 	
-	public void FindBY(String nom) {// fait une chercher par nom et pas par id
-	if (nom=="pierre") {System.out.println("ok c'est bon");}else {System.out.println("c'est pas bon");}
-	}
+	public String FindBY(String nom) {// fait une chercher par nom 
+	if (nom=="Les Misérables") {return "ok c'est bon";}
+	return null;}
+	// commentaire qui precise, pas d'indentation, nom fonction pas explicite
 	
-	public String Find(String id) {// fait une chercher par id
-	id = null;
-	return id;
-	}
+	public void Find() {// appel une fonction de recherche
+	String id="";
+	String res= FindBY(id);
+	System.out.println(res.toString());
+	}// ne gère pas les erreurs
 
 	
 	//correction
@@ -24,7 +26,7 @@ public class Example_code_for_clean {
 	//char pageMilieu ='5';
 	//char pageFin='9';	
 	
-	//public void FindBYNom(String nom) {
+	//public String FindBYNom(String nom) {
 	//if (nom=="pierre") {
 	//	System.out.println("ok c'est bon");
 	//}
@@ -36,7 +38,7 @@ public class Example_code_for_clean {
 	//public String FindById(String id) {
 	//	try {
 	//		id = null;
-	//		return id;
+	//		return FindByNom(id);
 	//	}catch (Error ex){
 	//		String messerror = "id null";
 	//		return messerror;
